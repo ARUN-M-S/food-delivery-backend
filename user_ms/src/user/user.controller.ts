867f99a7410e99d1147ca4429027fce7db07e857
ARUN-M-S <arunms96official@gmail.com>
2025-07-18 22:33:33 +0530

    bff to microservice connected successfully

--- user_ms/src/user/user.controller.ts
+++ user_ms/src/user/user.controller.ts
@@ -3,7 +3,7 @@ import { MessagePattern, Payload } from '@nestjs/microservices';
 
 @Controller('user')
 export class UserController {
-    @MessagePattern('user.ping')
+    @MessagePattern('user.create')
     handlePing(@Payload() message: any) {
       console.log('🔥 Received Kafka message on topic "user.ping":', message);
       return 'Ping received';
