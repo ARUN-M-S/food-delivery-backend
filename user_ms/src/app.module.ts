4274005a934265bb6d9790633618e438048249b4
ARUN-M-S <arunms96official@gmail.com>
2025-07-19 15:18:20 +0530

    Mongodb connection is completed

--- user_ms/src/app.module.ts
+++ user_ms/src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { HealthController } from './user/health.controller';
 import { UserModule } from './user/user.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ConfigModule } from '@nestjs/config';
+console.log('Loaded MONGO_URL:', process.env.MONGO_URL);
 
 
 @Module({
-  imports: [UserModule],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    MongooseModule.forRoot(process.env.MONGO_URL || ''),
+    UserModule],
   controllers: [HealthController],
   providers: [],
 })
