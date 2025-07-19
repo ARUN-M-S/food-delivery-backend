26c890661982ebfe48d759b39af48376ee4f3c34
ARUN-M-S <arunms96official@gmail.com>
2025-07-19 21:21:31 +0530

    password encryption done

--- user_ms/src/user/user.service.ts
+++ user_ms/src/user/user.service.ts
@@ -3,7 +3,7 @@ import { CreateCustomerDto } from 'src/common/dtos/create-customer.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Customer } from '../common/schemas/customer.schema';
 import { Model } from 'mongoose';
-import { retry } from 'rxjs';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UserService {
@@ -13,10 +13,12 @@ export class UserService {
     async createCustomer(data:CreateCustomerDto){
         try {
             const existing =  await this.customerModel.find({email:data?.email})
-            if(existing){
+           console.log(existing,"existing")
+            if(existing.length>0){
                 return { error: 'Email already exists' };
             }
-            const Customer = new this.customerModel(data);
+            const hashedPassword = await bcrypt.hash(data.password, 10);
+            const Customer = new this.customerModel({...data,password:hashedPassword});
             const result = await Customer.save();
             return {user:result}
         } catch (error) {
