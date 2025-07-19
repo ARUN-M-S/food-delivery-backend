0849f1cd0153b7538473d2407388fbeee18dec2e
ARUN-M-S <arunms96official@gmail.com>
2025-07-19 21:04:54 +0530

    duplicate email error handled in user ms

--- user_ms/src/user/user.service.ts
+++ user_ms/src/user/user.service.ts
@@ -3,6 +3,7 @@ import { CreateCustomerDto } from 'src/common/dtos/create-customer.dto';
 import { InjectModel } from '@nestjs/mongoose';
 import { Customer } from '../common/schemas/customer.schema';
 import { Model } from 'mongoose';
+import { retry } from 'rxjs';
 
 @Injectable()
 export class UserService {
@@ -10,9 +11,19 @@ export class UserService {
         @InjectModel(Customer.name) private customerModel: Model<Customer>,
       ) {}
     async createCustomer(data:CreateCustomerDto){
-        const Customer = new this.customerModel(data);
-        const result = await Customer.save();
-        console.log(result,"Customer")
-        return result
+        try {
+            const existing =  await this.customerModel.find({email:data?.email})
+            if(existing){
+                return { error: 'Email already exists' };
+            }
+            const Customer = new this.customerModel(data);
+            const result = await Customer.save();
+            console.log(result,"Customer")
+            return result
+        } catch (error) {
+            console.error('❌ User creation failed:', error.message);
+            return { error: 'Internal server error' };
+        }
+       
     }
 }
